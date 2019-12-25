c19bb5251b0c5236db3b0134129f96f3b30a60a6
Gor06 <59112809+Gor06@users.noreply.github.com>
2019-12-25 19:11:13 +0400

    About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <v-container id="about">
+    <h1 class="text-center" style="font-family: algerian; font-size:60px;">About</h1>
+    <p></p>
+    <v-row>
+    <v-col cols="12" md ="4">
+        <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
+          <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
+            <v-card-title style="font-family: cursive;">Vahe Khachatryan</v-card-title>
+          </v-img>
+          <v-card color="#D6D1D1" class="ma-7">
+            <v-card-text
+  class="text--primary"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora inventore debitis corporis, blanditiis totam maxime placeat iste veniam quo maiores non? Voluptates molestias libero labore distinctio accusantium temporibus quaerat. Fuga?</v-card-text>
+          </v-card>
+          <v-card-actions>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12"  md ="4">
+        <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
+          <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
+            <v-card-title style="font-family: cursive;">Gor Margaryan</v-card-title>
+          </v-img>
+          <v-card color="#D6D1D1" class="ma-7">
+            <v-card-text
+  class="text--primary"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora inventore debitis corporis, blanditiis totam maxime placeat iste veniam quo maiores non? Voluptates molestias libero labore distinctio accusantium temporibus quaerat. Fuga?</v-card-text>
+          </v-card>
+          <v-card-actions>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12"  md="4">
+        <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
+          <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
+            <v-card-title style="font-family: cursive;">Ani Avetisyan</v-card-title>
+          </v-img>
+          <v-card color="#D6D1D1" class="ma-7">
+            <v-card-text
+  class="text--primary"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora inventore debitis corporis, blanditiis totam maxime placeat iste veniam quo maiores non? Voluptates molestias libero labore distinctio accusantium temporibus quaerat. Fuga?</v-card-text>
+          </v-card>
+          <v-card-actions>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      </v-row>
   </v-container>
 </template>
 
