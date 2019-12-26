8515cd66224176da90a07ad01c18f6de92435460
user <vahe.khachatryan.y@tumo.org>
2019-12-26 19:19:24 +0400

    Responsivness

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="about">
-    <h1 class="text-center" style="font-family: algerian; font-size:60px;">About</h1>
-    <p>This site contains a wealth of information on various branches of Armenian art. It will take you to a unique world of art, where you will tangibly feel a centuries-old breath of Armenian art. You will be with the artists who bridge the past and present, you will know all about their period of life and left inharitance. Here you will find information not only about spiritual treasures, but also monuments representing the flight of armenian architectural thought, such as Zvartnots Cathedral, Republic Square, Opera and so on. So here is armenian art in all its luxury.
-</p>
+<h1 class="text-center" style="font-family: algerian; font-size:60px;">Our Mission</h1>
+<v-row>
+  <v-col cols="12" xl="7" lg="6">
+    <p class = "mx-12">This site contains a wealth of information on various branches of Armenian art. It will take you to a unique world of art, where you will tangibly feel a centuries-old breath of Armenian art. You will be with the artists who bridge the past and present, you will know all about their period of life and left inharitance. Here you will find information not only about spiritual treasures, but also monuments representing the flight of armenian architectural thought, such as Zvartnots Cathedral, Republic Square, Opera and so on. So here is armenian art in all its luxury.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum aliquet libero in consequat. Maecenas ut ligula et est commodo lacinia in et ante. In mattis eu mi a aliquam. Aliquam ac est leo. Curabitur elementum ipsum ut pharetra feugiat. Aenean pretium ligula massa, ut ultrices ipsum ultricies at. Nullam sagittis egestas dapibus. Phasellus vestibulum, lacus eget viverra ultricies, eros nisi dignissim velit, non blandit nulla ligula et nunc. Nullam malesuada diam a convallis facilisis. Cras at leo congue, mollis orci ac, lacinia augue.
+    </p>
+  </v-col>
+  <v-col cols="12" xl="5" lg="6">
+    <v-img src = "@/assets/img/about/TUMO.jpg"></v-img>
+  </v-col>
+</v-row>
 
-         <h1 class="text-center" style="font-family: algerian; font-size:60px;">Our Team</h1>
+
+
+
+  <h1 class="text-center" style="font-family: algerian; font-size:60px;">Our Team</h1>
     <v-row>
-    <v-col cols="12" md ="4">
+    <v-col cols="12" lg ="4" md="6" sm="12">
         <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
           <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
             <v-card-title style="font-family: cursive;">Vahe Khachatryan</v-card-title>
@@ -20,7 +31,7 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12"  md ="4">
+      <v-col cols="12"  lg ="4" md="6" sm="12">
         <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
           <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
             <v-card-title style="font-family: cursive;">Gor Margaryan</v-card-title>
@@ -34,7 +45,7 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12"  md="4">
+      <v-col cols="12"  lg="4" md="12" sm="12">
         <v-card color= "#ABABAB" class="mx-auto overlay" max-width="400">
           <v-img class="white--text align-end" height="260px" src="../assets/img/cards/logo.png">
             <v-card-title style="font-family: cursive;">Ani Avetisyan</v-card-title>
