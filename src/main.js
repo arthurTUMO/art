ee7a50823625089a9720a26419aa7986920a19b5
user <vahe.khachatryan.y@tumo.org>
2019-12-27 17:10:51 +0400

    Map

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "process.env.VUE_APP_GOOGLE_API_KEY",
+    libraries: "places" // necessary for places input
+  }
+});
 
 new Vue({
   vuetify,
   router,
+  el: "#app",
+  components: { App },
+  template: "<App/>",
   render: h => h(App)
 }).$mount('#app')
