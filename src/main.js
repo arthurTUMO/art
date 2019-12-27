81e1032ccffa85441ac32bf1179757462a414716
karine-minasyan <karine.minasyan@tumo.org>
2019-12-27 16:15:57 +0400

    add server, build for heroku

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  axios,
+  VueAxios,
   render: h => h(App)
 }).$mount('#app')
