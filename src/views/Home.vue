540d378fc25d634fb2f8d38cd79065b6dcc2e9c9
Arthur Kasumyan <arthurkasumyan@gmail.com>
2020-01-07 14:41:28 +0400

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,37 +7,33 @@
       continuous
       cycle
       interval="10000"
-      touch
       transition="none"
     >
       <v-carousel-item
-        lazy
         v-for="(item,i) in items"
         :key="i"
         :src="item.src"
         transition="scroll-x-transition"
         reverse-transition="scroll-x-reverse-transition"
       >
-       <v-jumbotron dark>
-            <v-container fill-height>
-              <v-layout align-center>
-                <v-flex :class="{'ma-12': $vuetify.breakpoint.smAndDown, 'mx-5': $vuetify.breakpoint.mdAndUp}">
-                  <h2 :class="{'display-1': $vuetify.breakpoint.smAndDown, 'display-3': $vuetify.breakpoint.mdAndUp}" class = "white-text">{{ item.title }}</h2>
-                  <h3 :class="{'display-0': $vuetify.breakpoint.smAndDown, 'display-1': $vuetify.breakpoint.mdAndUp}" class="subheading white-text">{{ item.description }}</h3>
-                  <v-col cols="12" sm="3">
-            <v-btn @click="$vuetify.goTo(1000, options)" block rounded dark x-large icon>
-              <v-icon>mdi-arrow-down</v-icon>
-            </v-btn>
-          </v-col>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-jumbotron>
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex :class="{'ma-12': $vuetify.breakpoint.smAndDown, 'mx-5': $vuetify.breakpoint.mdAndUp}">
+              <h2 :class="{'display-1': $vuetify.breakpoint.smAndDown, 'display-3': $vuetify.breakpoint.mdAndUp}" class = "white-text">{{ item.title }}</h2>
+              <h3 :class="{'display-0': $vuetify.breakpoint.smAndDown, 'display-1': $vuetify.breakpoint.mdAndUp}" class="subheading white-text">{{ item.description }}</h3>
+              <v-col cols="12" sm="3">
+                <v-btn @click="$vuetify.goTo(1000, options)" block rounded dark x-large icon>
+                  <v-icon>mdi-arrow-down</v-icon>
+                </v-btn>
+              </v-col>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-carousel-item>
     </v-carousel>
     <div class="divider">
-      <marquee v-for="fact in facts" :key="fact.id" :class="{'display-0': $vuetify.breakpoint.smAndDown, 'display-1': $vuetify.breakpoint.smAndUp}" class = "text-uppercase my-2" behavior="scroll" direction="left" scrollamount="18">{{ fact.text }}
-</marquee>
+      <marquee v-for="fact in facts" :key="fact.id" :class="{'display-0': $vuetify.breakpoint.smAndDown, 'display-1': $vuetify.breakpoint.smAndUp}" class = "text-uppercase mt-2 py-3" behavior="scroll" direction="left" scrolldelay="90">{{ fact.text }}
+      </marquee>
     </div>
       <v-parallax
            height="300"
@@ -47,19 +43,19 @@
     <v-container>
       <Cards/>
     </v-container>
-            <v-btn
-            v-scroll="onScroll"
-            v-show="fab"
-            fab
-            dark
-            fixed
-            bottom
-            right
-            color="primary"
-            @click="toTop"
-          >
-            <v-icon>mdi-arrow-up</v-icon>
-          </v-btn>
+    <v-btn
+    v-scroll="onScroll"
+    v-show="fab"
+    fab
+    dark
+    fixed
+    bottom
+    right
+    color="primary"
+    @click="toTop"
+  >
+    <v-icon>mdi-arrow-up</v-icon>
+  </v-btn>
   </div>
 </template>
 
@@ -74,27 +70,27 @@ export default {
       {
         src: require('../assets/img/slider/architecture.jpg'),
         title: 'Armenian Architecture',
-        description: 'Read More in this website'
+        description: 'Read more below'
       },
       {
         src: require('../assets/img/slider/literature.jpg'),
         title: 'Armenian Literature',
-        description: 'Read More in this website'
+        description: 'Read more below'
       },
       {
         src: require('../assets/img/slider/music.jpg'),
         title: 'Armenian Music',
-        description: 'Read More in this website'
+        description: 'Read more below'
       },
       {
         src: require('../assets/img/slider/carpet.png'),
         title: 'Armenian Carpets',
-        description: 'Read More in this website'
+        description: 'Read more below'
       }
     ],
     facts: [
       {
-        id: 1, text: 'Interesting fact: Armenia was the first country to have adopted Christianity as its state religion'
+        id: 1, text: 'Did you know? Armenia was the first country to adopt Christianity as its official, state religion.'
       }
     ]
   }),
